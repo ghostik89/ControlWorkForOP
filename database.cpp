0ac0769e86c83978dc93f1bed4827d2b53aa527b
@Lady_Proger <facktartar@gmail.com>
2019-12-21 01:01:10 +0400

    Сделана 3 лаба

--- database.cpp
+++ database.cpp
@@ -9,29 +9,32 @@ int DataBase::get_current_row(Student current){
     int index = 0;
     if(position.empty())
         return index;
-    //todo: сделать лучше
-    for (auto iter = position.begin(); iter != position.end(); iter++) {
-        auto pos = main_base.find(*iter);
+    QVectorIterator<int> iter(position);
+    bool next = true;
+    while(iter.hasNext()) {
+        auto pos = main_base.find(iter.next());
+        next = Student::compare_students(pos.value(), current);
         if(Student::compare_students(pos.value(), current))
             index++;
-        else
-            break;
     }
     return index;
 }
 Student DataBase::get_current_student(int id){  return main_base.find(id).value();   }
 
 int DataBase::count(){ return position.size(); }
+
 void DataBase::set_student(Student current, int id){
     main_base.insert(id, current);
     position.insert(position.begin() + id, id);
     modified = true;
 }
+
 void DataBase::delete_student(int id){
     main_base.erase(main_base.find(id));
     position.erase(position.begin() + id);
     modified = true;
 }
+
 void DataBase::update_db(Student current, int id){
     auto iter = main_base.find(id);
     iter.value() = current;
@@ -45,11 +48,11 @@ void DataBase::clear(){
     position.clear();
     modified = true;
 }
+
 bool DataBase::be_modified(){ return modified;  }
-void DataBase::sort(){}
 
 QDataStream& DataBase::operator<<(QDataStream& out){
-//    for (auto iter = main_base.begin(); iter != main_base.end(); iter++)
-//        out << iter.key() <<  iter.value();
+    for (auto iter = main_base.begin(); iter != main_base.end(); iter++)
+        out << iter.key() <<  iter.value();
 }
 QDataStream& DataBase::operator>>(QDataStream& in){}
